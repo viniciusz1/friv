d25064693adda61e93f1d97b14b36e9efad292a0
BERNARDO WEHMUTH <bernardo.wehmuth@catolicasc.edu.br>
2024-07-01 21:44:53 -0300

    tela do jogo

--- src/app/jogo/[idJogo]/page.jsx
+++ src/app/jogo/[idJogo]/page.jsx
@@ -1,21 +1,27 @@
 "use client"
-import { useEffect } from "react"
+import { useEffect,useState } from "react"
 import "../styled.css"
+import { games } from "@/constants"
 
-const Jogo = ({ idProjeto }) => {
+const Jogo = ({ params }) => {
+    const [jogo, setJogo] = useState();
     useEffect(()=>{
-        console.log(idProjeto)
+        const fitrados = games.find(game => {
+            return game.id == params.idJogo
+        })
+        setJogo(fitrados)
+
     },[])
     return (
         <>
             <div className="geral">
-                <h1 className="titulo">Jogo</h1>
+                <h1 className="titulo">{jogo?.name}</h1>
                 <div className="flex coisas">
                     <div className="w-3/4 tudo">
-                        <div className="telaJogo">TELA BOA TARDE</div>
+                        <div className="telaJogo" style={{"backgroundImage": `url(${jogo?.largeImage})`}}></div>
                         <div className="interativos">botao</div>
                     </div>
-                    <div className="info w-1/4">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam, voluptate temporibus. Laborum, temporibus omnis dicta odio eius et possimus quibusdam, libero voluptatibus quis quam est cumque atque at error. Itaque!</div>
+                    <div className="info w-1/4">{jogo?.description}</div>
 
                 </div>
             </div>
--- src/constants/index.js
+++ src/constants/index.js
@@ -2,61 +2,85 @@ export const games = [
     {
         "id": 1,
         "name": "Jogo 1",
-        "image": "https://www.friv.com/z/thumbs/4colors360x100Min.png"
+        "image": "https://www.friv.com/z/thumbs/4colors360x100Min.png",
+        "largeImage": "https://images.crazygames.com/games/uno-online/cover-1679068977831.png?auto=format,compress&q=75&cs=strip",
+        "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab, laborum quisquam? Perspiciatis ex culpa provident aspernatur expedita accusantium. Est tempore molestiae tenetur accusantium, maiores quod quaerat mollitia et! Debitis, qui."
     },
     {
         "id": 2,
         "name": "Jogo 2",
-        "image": "https://www.friv.com/z/thumbs/basketchamps360x100Min.png"
+        "image": "https://www.friv.com/z/thumbs/basketchamps360x100Min.png",
+        "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab, laborum quisquam? Perspiciatis ex culpa provident aspernatur expedita accusantium. Est tempore molestiae tenetur accusantium, maiores quod quaerat mollitia et! Debitis, qui."
+
     },
     {
         "id": 3,
         "name": "Jogo 3",
-        "image": "https://www.friv.com/z/thumbs/powerpamplona360x100Min.png"
+        "image": "https://www.friv.com/z/thumbs/powerpamplona360x100Min.png",
+        "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab, laborum quisquam? Perspiciatis ex culpa provident aspernatur expedita accusantium. Est tempore molestiae tenetur accusantium, maiores quod quaerat mollitia et! Debitis, qui."
+
     },
     {
         "id": 4,
         "name": "Jogo 4",
         "image": "https://www.friv.com/z/thumbs/madfish360x100Min.png"
+        ,
+        "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab, laborum quisquam? Perspiciatis ex culpa provident aspernatur expedita accusantium. Est tempore molestiae tenetur accusantium, maiores quod quaerat mollitia et! Debitis, qui."
     },
     {
         "id": 5,
         "name": "Jogo 5",
         "image": "https://www.friv.com/z/thumbs/fireboyandwatergirlforest360x100Min.png"
+        ,
+        "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab, laborum quisquam? Perspiciatis ex culpa provident aspernatur expedita accusantium. Est tempore molestiae tenetur accusantium, maiores quod quaerat mollitia et! Debitis, qui."
     },
     {
         "id": 6,
         "name": "Jogo 6",
         "image": "https://www.friv.com/z/thumbs/frivsoccerheads360x100Min.png"
+        ,
+        "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab, laborum quisquam? Perspiciatis ex culpa provident aspernatur expedita accusantium. Est tempore molestiae tenetur accusantium, maiores quod quaerat mollitia et! Debitis, qui."
     },
     {
         "id": 7,
         "name": "Jogo 7",
         "image": "https://www.friv.com/z/thumbs/tictactoexxo360x100Min.png"
+        ,
+        "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab, laborum quisquam? Perspiciatis ex culpa provident aspernatur expedita accusantium. Est tempore molestiae tenetur accusantium, maiores quod quaerat mollitia et! Debitis, qui."
     },
     {
         "id": 8,
         "name": "Jogo 8",
         "image": "https://www.friv.com/z/thumbs/bloxorz360x100Min.png"
+        ,
+        "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab, laborum quisquam? Perspiciatis ex culpa provident aspernatur expedita accusantium. Est tempore molestiae tenetur accusantium, maiores quod quaerat mollitia et! Debitis, qui."
     },
     {
         "id": 9,
         "name": "Jogo 9",
         "image": "https://www.friv.com/z/thumbs/penaltychallenge360x100Min.png"
+        ,
+        "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab, laborum quisquam? Perspiciatis ex culpa provident aspernatur expedita accusantium. Est tempore molestiae tenetur accusantium, maiores quod quaerat mollitia et! Debitis, qui."
     },
     {
         "id": 10,
         "name": "Jogo 10",
         "image": "https://www.friv.com/z/thumbs/smilingglasspropourer360x100Min.png"
+        ,
+        "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab, laborum quisquam? Perspiciatis ex culpa provident aspernatur expedita accusantium. Est tempore molestiae tenetur accusantium, maiores quod quaerat mollitia et! Debitis, qui."
     },
     {
         "id": 11,
         "name": "Jogo 11",
         "image": "https://www.friv.com/z/thumbs/avoidmissiles360x100Min.png"
+        ,
+        "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab, laborum quisquam? Perspiciatis ex culpa provident aspernatur expedita accusantium. Est tempore molestiae tenetur accusantium, maiores quod quaerat mollitia et! Debitis, qui."
     },
     {
         "id": 12,
         "name": "Jogo 12",
         "image": "https://www.friv.com/z/thumbs/bobtherobber360x100Min.png"
+        ,
+        "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab, laborum quisquam? Perspiciatis ex culpa provident aspernatur expedita accusantium. Est tempore molestiae tenetur accusantium, maiores quod quaerat mollitia et! Debitis, qui."
     }
 ]
