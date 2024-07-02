d57194e90a60a5eea3b6a5b396568fa7cda7b9a6
Vinícius Bonatti Benner <vinicius101313@gmail.com>
2024-07-01 21:25:46 -0300

    .

---
+++ src/components/shared/AutoComplete/Autocomplete.jsx
@@ -0,0 +1,52 @@
+"use client"
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import { games } from '@/constants';
+
+export default function AutocompleteJogo() {
+  return (
+    <Autocomplete
+      id="country-select-demo"
+      sx={{ width: 300 }}
+      options={games}
+      autoHighlight
+      getOptionLabel={(option) => option.name}
+      renderOption={(props, option) => {
+        const { key, ...optionProps } = props;
+        return (
+          <Box
+            key={key}
+            component="li"
+            sx={{ '& > img': { mr: 2, flexShrink: 0 } }}
+            {...optionProps}
+          >
+            <img
+              loading="lazy"
+              width="170"
+              src={option.image}
+              alt=""
+            />
+            {option.name}
+          </Box>
+        );
+      }}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          size='small'
+          label="Pesquisar jogo..."
+          onClick={(e) => {
+            
+          }}
+          
+          inputProps={{
+            ...params.inputProps,
+            autoComplete: 'new-password', // disable autocomplete and autofill
+          }}
+        />
+      )}
+    />
+  );
+}
--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,11 +1,13 @@
 import { Input } from "../ui/input";
-
+import AutocompleteJogo from "../shared/Autocomplete/Autocomplete";
 const Header = () => {
     return (
         <div className="flex h-20 justify-between p-5">
-            <div>resto</div>
+            <div>
+                <img src="../logofriv-removebg-preview.png" alt="" />
+            </div>
             <div className="flex">
-                <Input type="text" placeholder="Pesquise um jogo..." />
+                <AutocompleteJogo type="text" placeholder="Pesquise um jogo..." />
                 <button>Filtrar</button>
 
             </div>
