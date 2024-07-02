b190037c2496f1b9cba5efedb6bae8f3d9124e19
NICOLAS SOARES OLIVEIRA <nicolas.oliveira@catolicasc.edu.br>
2024-07-01 21:23:44 -0300

    '.'

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -3,7 +3,7 @@ import { Input } from "../ui/input";
 const Header = () => {
     return (
         <div className="flex h-20 justify-between p-5">
-            <div>resto</div>
+            <img src="../logofriv-removebg-preview.png" alt="" />
             <div className="flex">
                 <Input type="text" placeholder="Pesquise um jogo..." />
                 <button>Filtrar</button>
