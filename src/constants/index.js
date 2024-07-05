3826d4f4b447e6b891a4cb97ddf8c9c9195fdd5a
Vinicius Bueno <viniciusbuenodeoliveira2017@gmail.com>
2024-07-05 20:51:24 -0300

    .

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,7 +3,7 @@ export const games = [
         "id": 1,
         "name": "Jogo 1",
         "image": "https://www.friv.com/z/thumbs/4colors360x100Min.png",
-        "largeImage": "https://cdn.gencraft.com/prod/user/d60f97bf-626e-4ab5-a90e-f31c8fe3b832/34196947-e87c-4b01-8eb4-89c3a7ea25c1/image/image1_0.jpg?Expires=1720193634&Signature=PRMvKSJjSGYFs7Tdq4E5qD6fIDJtm0enAYOYjk7xVaKBxKGHznSI0tZaykIKDJ6j6~3n5vxjP-hOtf-gH1Q03or9QLeXuh9aletWlfgS7sWMOETn9Hin3pmTl2yz7wA-DZ2nFaognnDnkd3d8XKkoPtzqgskpaXX1K63IwcIe8ht2M~Rs1Dd4zVZHsRHF7NRPHTtPSqd80ODshULCslzk9o76mTeShxFVBRfDAAKwnJNGFteDguWYBVmJId-k1IvbZvl~0CBDxOmrXsNWwpmV4un5i5sLPHYYw2MZ7-ND-3oZ~ed4n2ILv8FmUAkgGsnou2Pm-1jbSgNJ0uvoHi6KQ__&Key-Pair-Id=K3RDDB1TZ8BHT8"
+        "largeImage": "https://placehold.co/1200x800/003/003/png"
         ,
         "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab, laborum quisquam? Perspiciatis ex culpa provident aspernatur expedita accusantium. Est tempore molestiae tenetur accusantium, maiores quod quaerat mollitia et! Debitis, qui."
     },
@@ -11,7 +11,7 @@ export const games = [
         "id": 2,
         "name": "Jogo 2",
         "image": "https://www.friv.com/z/thumbs/basketchamps360x100Min.png",
-        "largeImage": "https://cdn.gencraft.com/prod/user/d60f97bf-626e-4ab5-a90e-f31c8fe3b832/34196947-e87c-4b01-8eb4-89c3a7ea25c1/image/image0_0.jpg?Expires=1720193634&Signature=LkkvaSItEv5mz-L3SqDoxcXphna6RLnSBXtTdZEi3poXWysL11Liq9C4mr377Cj5rO9rfVkAzciMTA0i2jWFediWoj3b8oqJXr7gDHhxHwuFcYX0eQZ6uCF0vVjehkztbMhpHy0oFDOxbyuhI767g1wM~ouR1mXtMPLHlOI7-k8jnFJ0OmpjzP1uZBWB0a68zFsWjlwAolGzbdZht0CB41feYZRvCMLYVAnlX-wcvIKX20eWsd2FPo2qHMWIM~eBZ8~O9t0g1SyCCimTIKmVO5Z2KYEyBwTes7oyFI5CUsnIyKwEFyTGuC~aJ3VeTTSz8pZPKzK1vvPW2WUaG-6sng__&Key-Pair-Id=K3RDDB1TZ8BHT8"
+        "largeImage": "https://placehold.co/1200x800/003/003/png"
         ,
         "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab, laborum quisquam? Perspiciatis ex culpa provident aspernatur expedita accusantium. Est tempore molestiae tenetur accusantium, maiores quod quaerat mollitia et! Debitis, qui."
 
@@ -20,7 +20,7 @@ export const games = [
         "id": 3,
         "name": "Jogo 3",
         "image": "https://www.friv.com/z/thumbs/powerpamplona360x100Min.png",
-        "largeImage": "https://cdn.gencraft.com/prod/user/d60f97bf-626e-4ab5-a90e-f31c8fe3b832/32569bf8-946e-46db-b4da-2afadf8c0274/image/image0_0.jpg?Expires=1720194023&Signature=Qmu1IIs0a6YHpKFZfh8k~qWJS0Nc7hqNZEwdyv86xFRNKlBwMdEtu57pKhZGSSjtwvbJx7QGcuMAqjLtJbEMCtXqu3bqkPR5Xjg6k-eepobOLhNNYNpD4QFAFxk22EfNgELLEBu~upyQs0yG1YhdBbsjWVoPD02BxVZqNr0W4kCHlHaQzAkc2vQobpvnXFsQ0zsdSKxEaVe1AxnTAMUBvBnL6xcz2jUgPI--iy-UHqVxtPUEPJYwDKEQSmvupi5JYHe-DoqSyAJ3mcBg08vTj10wVfs-ZFOzy4sUFQduU4gTidkcyIEHQGTmP99YbB7B3ADDO4ist0JqhhaUdbCzUQ__&Key-Pair-Id=K3RDDB1TZ8BHT8"
+        "largeImage": "https://placehold.co/1200x800/003/003/png"
         ,
         "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab, laborum quisquam? Perspiciatis ex culpa provident aspernatur expedita accusantium. Est tempore molestiae tenetur accusantium, maiores quod quaerat mollitia et! Debitis, qui."
 
@@ -29,7 +29,7 @@ export const games = [
         "id": 4,
         "name": "Jogo 4",
         "image": "https://www.friv.com/z/thumbs/madfish360x100Min.png",
-        "largeImage": "https://cdn.gencraft.com/prod/user/d60f97bf-626e-4ab5-a90e-f31c8fe3b832/417f7359-64bf-4ebe-8b37-ce3de4f2301f/image/image0_0.jpg?Expires=1720194087&Signature=oxAWjzp58VTz4e7d3N6uqIgyzHSqhWPa9qZpJU8eVCFtCcP-oc3wrXDme-k5tavlaU4EUToMtBkvn49vY9sKPX5DzQG0pJulnvY4-LOUzFSyjvkluEmDVgneQ48y~AnGpRy4umSLmNYubdZ~T~mXdnejLA6hu2vpWSRySQPI2WeYkTdGUoph3gQUuHueI~UWX3ve9hLA0v7Es31Bh2XDM7lqh0aFlCi6CK2afARPRKYN-3POjdXU00OAfuu-A8u~qHjcm9atJCD93zPIqM79Y7w8spEOSagH~0yFSf2W7Pg9gW4NN5Md7KZl-nQlqFo3fPMcrYMD5Aw5OLHoTW2I-Q__&Key-Pair-Id=K3RDDB1TZ8BHT8"
+        "largeImage": "https://placehold.co/1200x800/003/003/png"
         ,
         "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab, laborum quisquam? Perspiciatis ex culpa provident aspernatur expedita accusantium. Est tempore molestiae tenetur accusantium, maiores quod quaerat mollitia et! Debitis, qui."
     },
@@ -37,7 +37,7 @@ export const games = [
         "id": 5,
         "name": "Jogo 5",
         "image": "https://www.friv.com/z/thumbs/fireboyandwatergirlforest360x100Min.png",
-        "largeImage": "https://cdn.gencraft.com/prod/user/d60f97bf-626e-4ab5-a90e-f31c8fe3b832/34196947-e87c-4b01-8eb4-89c3a7ea25c1/image/image1_0.jpg?Expires=1720193634&Signature=PRMvKSJjSGYFs7Tdq4E5qD6fIDJtm0enAYOYjk7xVaKBxKGHznSI0tZaykIKDJ6j6~3n5vxjP-hOtf-gH1Q03or9QLeXuh9aletWlfgS7sWMOETn9Hin3pmTl2yz7wA-DZ2nFaognnDnkd3d8XKkoPtzqgskpaXX1K63IwcIe8ht2M~Rs1Dd4zVZHsRHF7NRPHTtPSqd80ODshULCslzk9o76mTeShxFVBRfDAAKwnJNGFteDguWYBVmJId-k1IvbZvl~0CBDxOmrXsNWwpmV4un5i5sLPHYYw2MZ7-ND-3oZ~ed4n2ILv8FmUAkgGsnou2Pm-1jbSgNJ0uvoHi6KQ__&Key-Pair-Id=K3RDDB1TZ8BHT8"
+        "largeImage": "https://placehold.co/1200x800/003/003/png"
         ,
         "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab, laborum quisquam? Perspiciatis ex culpa provident aspernatur expedita accusantium. Est tempore molestiae tenetur accusantium, maiores quod quaerat mollitia et! Debitis, qui."
     },
@@ -45,7 +45,7 @@ export const games = [
         "id": 6,
         "name": "Jogo 6",
         "image": "https://www.friv.com/z/thumbs/frivsoccerheads360x100Min.png",
-        "largeImage": "https://cdn.gencraft.com/prod/user/d60f97bf-626e-4ab5-a90e-f31c8fe3b832/34196947-e87c-4b01-8eb4-89c3a7ea25c1/image/image0_0.jpg?Expires=1720193634&Signature=LkkvaSItEv5mz-L3SqDoxcXphna6RLnSBXtTdZEi3poXWysL11Liq9C4mr377Cj5rO9rfVkAzciMTA0i2jWFediWoj3b8oqJXr7gDHhxHwuFcYX0eQZ6uCF0vVjehkztbMhpHy0oFDOxbyuhI767g1wM~ouR1mXtMPLHlOI7-k8jnFJ0OmpjzP1uZBWB0a68zFsWjlwAolGzbdZht0CB41feYZRvCMLYVAnlX-wcvIKX20eWsd2FPo2qHMWIM~eBZ8~O9t0g1SyCCimTIKmVO5Z2KYEyBwTes7oyFI5CUsnIyKwEFyTGuC~aJ3VeTTSz8pZPKzK1vvPW2WUaG-6sng__&Key-Pair-Id=K3RDDB1TZ8BHT8"
+        "largeImage": "https://placehold.co/1200x800/003/003/png"
         ,
         "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab, laborum quisquam? Perspiciatis ex culpa provident aspernatur expedita accusantium. Est tempore molestiae tenetur accusantium, maiores quod quaerat mollitia et! Debitis, qui."
     },
@@ -53,7 +53,7 @@ export const games = [
         "id": 7,
         "name": "Jogo 7",
         "image": "https://www.friv.com/z/thumbs/tictactoexxo360x100Min.png",
-        "largeImage": "https://cdn.gencraft.com/prod/user/d60f97bf-626e-4ab5-a90e-f31c8fe3b832/32569bf8-946e-46db-b4da-2afadf8c0274/image/image0_0.jpg?Expires=1720194023&Signature=Qmu1IIs0a6YHpKFZfh8k~qWJS0Nc7hqNZEwdyv86xFRNKlBwMdEtu57pKhZGSSjtwvbJx7QGcuMAqjLtJbEMCtXqu3bqkPR5Xjg6k-eepobOLhNNYNpD4QFAFxk22EfNgELLEBu~upyQs0yG1YhdBbsjWVoPD02BxVZqNr0W4kCHlHaQzAkc2vQobpvnXFsQ0zsdSKxEaVe1AxnTAMUBvBnL6xcz2jUgPI--iy-UHqVxtPUEPJYwDKEQSmvupi5JYHe-DoqSyAJ3mcBg08vTj10wVfs-ZFOzy4sUFQduU4gTidkcyIEHQGTmP99YbB7B3ADDO4ist0JqhhaUdbCzUQ__&Key-Pair-Id=K3RDDB1TZ8BHT8"
+        "largeImage": "https://placehold.co/1200x800/003/003/png"
         ,
         "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab, laborum quisquam? Perspiciatis ex culpa provident aspernatur expedita accusantium. Est tempore molestiae tenetur accusantium, maiores quod quaerat mollitia et! Debitis, qui."
     },
@@ -61,7 +61,7 @@ export const games = [
         "id": 8,
         "name": "Jogo 8",
         "image": "https://www.friv.com/z/thumbs/bloxorz360x100Min.png",
-        "largeImage": "https://cdn.gencraft.com/prod/user/d60f97bf-626e-4ab5-a90e-f31c8fe3b832/417f7359-64bf-4ebe-8b37-ce3de4f2301f/image/image0_0.jpg?Expires=1720194087&Signature=oxAWjzp58VTz4e7d3N6uqIgyzHSqhWPa9qZpJU8eVCFtCcP-oc3wrXDme-k5tavlaU4EUToMtBkvn49vY9sKPX5DzQG0pJulnvY4-LOUzFSyjvkluEmDVgneQ48y~AnGpRy4umSLmNYubdZ~T~mXdnejLA6hu2vpWSRySQPI2WeYkTdGUoph3gQUuHueI~UWX3ve9hLA0v7Es31Bh2XDM7lqh0aFlCi6CK2afARPRKYN-3POjdXU00OAfuu-A8u~qHjcm9atJCD93zPIqM79Y7w8spEOSagH~0yFSf2W7Pg9gW4NN5Md7KZl-nQlqFo3fPMcrYMD5Aw5OLHoTW2I-Q__&Key-Pair-Id=K3RDDB1TZ8BHT8"
+        "largeImage": "https://placehold.co/1200x800/003/003/png"
         ,
         "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab, laborum quisquam? Perspiciatis ex culpa provident aspernatur expedita accusantium. Est tempore molestiae tenetur accusantium, maiores quod quaerat mollitia et! Debitis, qui."
     },
@@ -69,7 +69,7 @@ export const games = [
         "id": 9,
         "name": "Jogo 9",
         "image": "https://www.friv.com/z/thumbs/penaltychallenge360x100Min.png",
-        "largeImage": "https://cdn.gencraft.com/prod/user/d60f97bf-626e-4ab5-a90e-f31c8fe3b832/34196947-e87c-4b01-8eb4-89c3a7ea25c1/image/image1_0.jpg?Expires=1720193634&Signature=PRMvKSJjSGYFs7Tdq4E5qD6fIDJtm0enAYOYjk7xVaKBxKGHznSI0tZaykIKDJ6j6~3n5vxjP-hOtf-gH1Q03or9QLeXuh9aletWlfgS7sWMOETn9Hin3pmTl2yz7wA-DZ2nFaognnDnkd3d8XKkoPtzqgskpaXX1K63IwcIe8ht2M~Rs1Dd4zVZHsRHF7NRPHTtPSqd80ODshULCslzk9o76mTeShxFVBRfDAAKwnJNGFteDguWYBVmJId-k1IvbZvl~0CBDxOmrXsNWwpmV4un5i5sLPHYYw2MZ7-ND-3oZ~ed4n2ILv8FmUAkgGsnou2Pm-1jbSgNJ0uvoHi6KQ__&Key-Pair-Id=K3RDDB1TZ8BHT8"
+        "largeImage": "https://placehold.co/1200x800/003/003/png"
         ,
         "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab, laborum quisquam? Perspiciatis ex culpa provident aspernatur expedita accusantium. Est tempore molestiae tenetur accusantium, maiores quod quaerat mollitia et! Debitis, qui."
     },
@@ -77,7 +77,7 @@ export const games = [
         "id": 10,
         "name": "Jogo 10",
         "image": "https://www.friv.com/z/thumbs/smilingglasspropourer360x100Min.png",
-        "largeImage": "https://cdn.gencraft.com/prod/user/d60f97bf-626e-4ab5-a90e-f31c8fe3b832/34196947-e87c-4b01-8eb4-89c3a7ea25c1/image/image0_0.jpg?Expires=1720193634&Signature=LkkvaSItEv5mz-L3SqDoxcXphna6RLnSBXtTdZEi3poXWysL11Liq9C4mr377Cj5rO9rfVkAzciMTA0i2jWFediWoj3b8oqJXr7gDHhxHwuFcYX0eQZ6uCF0vVjehkztbMhpHy0oFDOxbyuhI767g1wM~ouR1mXtMPLHlOI7-k8jnFJ0OmpjzP1uZBWB0a68zFsWjlwAolGzbdZht0CB41feYZRvCMLYVAnlX-wcvIKX20eWsd2FPo2qHMWIM~eBZ8~O9t0g1SyCCimTIKmVO5Z2KYEyBwTes7oyFI5CUsnIyKwEFyTGuC~aJ3VeTTSz8pZPKzK1vvPW2WUaG-6sng__&Key-Pair-Id=K3RDDB1TZ8BHT8"
+        "largeImage": "https://placehold.co/1200x800/003/003/png"
         ,
         "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab, laborum quisquam? Perspiciatis ex culpa provident aspernatur expedita accusantium. Est tempore molestiae tenetur accusantium, maiores quod quaerat mollitia et! Debitis, qui."
     },
@@ -85,7 +85,7 @@ export const games = [
         "id": 11,
         "name": "Jogo 11",
         "image": "https://www.friv.com/z/thumbs/avoidmissiles360x100Min.png",
-        "largeImage": "https://cdn.gencraft.com/prod/user/d60f97bf-626e-4ab5-a90e-f31c8fe3b832/32569bf8-946e-46db-b4da-2afadf8c0274/image/image0_0.jpg?Expires=1720194023&Signature=Qmu1IIs0a6YHpKFZfh8k~qWJS0Nc7hqNZEwdyv86xFRNKlBwMdEtu57pKhZGSSjtwvbJx7QGcuMAqjLtJbEMCtXqu3bqkPR5Xjg6k-eepobOLhNNYNpD4QFAFxk22EfNgELLEBu~upyQs0yG1YhdBbsjWVoPD02BxVZqNr0W4kCHlHaQzAkc2vQobpvnXFsQ0zsdSKxEaVe1AxnTAMUBvBnL6xcz2jUgPI--iy-UHqVxtPUEPJYwDKEQSmvupi5JYHe-DoqSyAJ3mcBg08vTj10wVfs-ZFOzy4sUFQduU4gTidkcyIEHQGTmP99YbB7B3ADDO4ist0JqhhaUdbCzUQ__&Key-Pair-Id=K3RDDB1TZ8BHT8"
+        "largeImage": "https://placehold.co/1200x800/003/003/png"
         ,
         "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab, laborum quisquam? Perspiciatis ex culpa provident aspernatur expedita accusantium. Est tempore molestiae tenetur accusantium, maiores quod quaerat mollitia et! Debitis, qui."
     },
@@ -93,7 +93,7 @@ export const games = [
         "id": 12,
         "name": "Jogo 12",
         "image": "https://www.friv.com/z/thumbs/bobtherobber360x100Min.png",
-        "largeImage": "https://cdn.gencraft.com/prod/user/d60f97bf-626e-4ab5-a90e-f31c8fe3b832/417f7359-64bf-4ebe-8b37-ce3de4f2301f/image/image0_0.jpg?Expires=1720194087&Signature=oxAWjzp58VTz4e7d3N6uqIgyzHSqhWPa9qZpJU8eVCFtCcP-oc3wrXDme-k5tavlaU4EUToMtBkvn49vY9sKPX5DzQG0pJulnvY4-LOUzFSyjvkluEmDVgneQ48y~AnGpRy4umSLmNYubdZ~T~mXdnejLA6hu2vpWSRySQPI2WeYkTdGUoph3gQUuHueI~UWX3ve9hLA0v7Es31Bh2XDM7lqh0aFlCi6CK2afARPRKYN-3POjdXU00OAfuu-A8u~qHjcm9atJCD93zPIqM79Y7w8spEOSagH~0yFSf2W7Pg9gW4NN5Md7KZl-nQlqFo3fPMcrYMD5Aw5OLHoTW2I-Q__&Key-Pair-Id=K3RDDB1TZ8BHT8"
+        "largeImage": "https://placehold.co/1200x800/003/003/png"
         ,
         "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab, laborum quisquam? Perspiciatis ex culpa provident aspernatur expedita accusantium. Est tempore molestiae tenetur accusantium, maiores quod quaerat mollitia et! Debitis, qui."
     }
